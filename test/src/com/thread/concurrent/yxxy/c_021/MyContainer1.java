0990186a7d2b16782ec01558a1403ce64b5b9cc5
youjingdike <82329839@qq.com>
2019-11-18 19:14:27 +0800

    提交新文件

--- test/src/com/thread/concurrent/yxxy/c_021/MyContainer1.java
+++ test/src/com/thread/concurrent/yxxy/c_021/MyContainer1.java
@@ -13,12 +13,12 @@ import java.util.concurrent.TimeUnit;
 
 public class MyContainer1<T> {
 	final private LinkedList<T> lists = new LinkedList<>();
-	final private int MAX = 10; //���10��Ԫ��
+	final private int MAX = 10; //最多10个元素
 	private int count = 0;
 	
 	
 	public synchronized void put(T t) {
-		while(lists.size() == MAX) { //����Ϊʲô��while��������if��
+		while(lists.size() == MAX) { //想想为什么用while而不是用if？
 			try {
 				this.wait(); //effective java
 			} catch (InterruptedException e) {
@@ -28,7 +28,7 @@ public class MyContainer1<T> {
 		
 		lists.add(t);
 		++count;
-		this.notifyAll(); //֪ͨ�������߳̽�������
+		this.notifyAll(); //通知所有线程
 	}
 	
 	public synchronized T get() {
@@ -42,13 +42,13 @@ public class MyContainer1<T> {
 		}
 		t = lists.removeFirst();
 		count --;
-		this.notifyAll(); //֪ͨ�����߽�������
+		this.notifyAll(); //通知所有线程
 		return t;
 	}
 	
 	public static void main(String[] args) {
 		MyContainer1<String> c = new MyContainer1<>();
-		//�����������߳�
+		//启动消费者线程
 		for(int i=0; i<10; i++) {
 			new Thread(()->{
 				for(int j=0; j<5; j++) System.out.println(c.get());
@@ -61,7 +61,7 @@ public class MyContainer1<T> {
 			e.printStackTrace();
 		}
 		
-		//�����������߳�
+		//启动生产者线程
 		for(int i=0; i<2; i++) {
 			new Thread(()->{
 				for(int j=0; j<25; j++) c.put(Thread.currentThread().getName() + " " + j);
