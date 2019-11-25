0806fc1f963359f671f36de9c4fc94a0616e48ba
youjingdike <82329839@qq.com>
2019-11-25 15:53:21 +0800

    提交新文件

---
+++ test/src/com/thread/ThisEscape.java
@@ -0,0 +1,14 @@
+package com.thread;
+
+import java.awt.*;
+import java.util.EventListener;
+
+public class ThisEscape {
+    /*public ThisEscape(EventSource source) {
+        source.registerListener(new EventListener() {
+            public void onEvent(Event e) {
+//                doSomething(e);
+            }
+        });
+    }*/
+}
