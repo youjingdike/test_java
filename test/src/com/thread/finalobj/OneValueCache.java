2b1bec4d72e90832a0ddd00d81f69741f80100eb
youjingdike <82329839@qq.com>
2019-11-27 09:27:37 +0800

    提交新文件

---
+++ test/src/com/thread/finalobj/OneValueCache.java
@@ -0,0 +1,29 @@
+package com.thread.finalobj;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+@Immutable
+public class OneValueCache {
+    private final BigInteger lastNumber;
+    private final BigInteger[] lastFactors;
+
+
+    public OneValueCache(BigInteger i, BigInteger[] factors) {
+        this.lastNumber = i;
+        this.lastFactors = Arrays.copyOf(factors,factors.length);
+    }
+
+    public BigInteger[] getFactors(BigInteger integer) {
+        if (lastNumber ==  null || !lastNumber.equals(integer)) {
+            return null;
+        } else {
+            return Arrays.copyOf(lastFactors, lastFactors.length);
+        }
+
+    }
+
+}
