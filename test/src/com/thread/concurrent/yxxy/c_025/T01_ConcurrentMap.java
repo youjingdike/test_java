cbb79a12adf4a26f95711e31c86cc30e8a263ec2
youjingdike <82329839@qq.com>
2019-12-02 18:39:16 +0800

    提交新文件

--- test/src/com/thread/concurrent/yxxy/c_025/T01_ConcurrentMap.java
+++ test/src/com/thread/concurrent/yxxy/c_025/T01_ConcurrentMap.java
@@ -6,20 +6,23 @@
 package com.thread.concurrent.yxxy.c_025;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.CountDownLatch;
 
 public class T01_ConcurrentMap {
 	public static void main(String[] args) {
-		//Map<String, String> map = new ConcurrentHashMap<>();
-		Map<String, String> map = new ConcurrentSkipListMap<>(); //高并发并且排序
+		Map<String, String> map = new ConcurrentHashMap<>();
+//		Map<String, String> map = new ConcurrentSkipListMap<>(); //高并发并且排序
 
-		//Map<String, String> map = new Hashtable<>();
-		//Map<String, String> map = new HashMap<>(); //Collections.synchronizedXXX
+//		Map<String, String> map = new Hashtable<>();
+//		Map<String, String> map = new HashMap<>(); //Collections.synchronizedXXX
 		//TreeMap
 		Random r = new Random();
 		Thread[] ths = new Thread[100];
@@ -41,5 +44,14 @@ public class T01_ConcurrentMap {
 
 		long end = System.currentTimeMillis();
 		System.out.println(end - start);
+		ConcurrentMap<String,String> map1 = new ConcurrentHashMap<>();
+		map1.put("1","1");
+		Map<String, String> map2 = Collections.unmodifiableMap(map1);
+		System.out.println(map1);
+		System.out.println(map2);
+
+		map1.put("1","2");
+		System.out.println(map1);
+		System.out.println(map2);
 	}
 }
